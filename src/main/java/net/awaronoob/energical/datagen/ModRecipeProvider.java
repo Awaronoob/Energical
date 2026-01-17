530b4f7506cbda7ae51fed0420c3fcf151c4ea9c
awaronoob <awaro.b@protonmail.com>
2026-01-17 15:31:50 +0100

    Finishing datagen 6.

--- src/main/java/net/awaronoob/energical/datagen/ModRecipeProvider.java
+++ src/main/java/net/awaronoob/energical/datagen/ModRecipeProvider.java
@@ -1,15 +1,17 @@
 package net.awaronoob.energical.datagen;
 
+import net.awaronoob.energical.block.ModBlocks;
 import net.awaronoob.energical.item.ModItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
-import net.minecraft.data.recipes.RecipeCategory;
-import net.minecraft.data.recipes.RecipeOutput;
-import net.minecraft.data.recipes.RecipeProvider;
-import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraft.data.recipes.*;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class ModRecipeProvider extends RecipeProvider implements IConditionBuilder {
@@ -19,8 +21,11 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
 
     @Override
     protected void buildRecipes(RecipeOutput pRecipeOutput) {
+        List<ItemLike> NEODYMITE_SMELTABLES = List.of(ModItems.RAW_NEODYMITE.get(), ModBlocks.NEODYMITE_ORE.get(),
+                ModBlocks.DEEPSLATE_NEODYMITE_ORE.get());
 
-        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.UNPRESSED_FLUXXITE.get())
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.UNPRESSED_FLUXXITE.get(), 3)
                 .pattern("GND")
                 .pattern("CID")
                 .pattern("DDD")
@@ -30,6 +35,14 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
                 .define('I', Items.IRON_INGOT)
                 .define('D', Items.DIAMOND)
                 .unlockedBy(getHasName(ModItems.NEODYMITE_INGOT.get()), has(ModItems.NEODYMITE_INGOT.get())).save(pRecipeOutput);
+
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC,ModItems.NEODYMITE_INGOT.get(), 9)
+                .requires(ItemTags.COAL_ORES)
+                .unlockedBy(getHasName(Blocks.COAL_ORE), has(Blocks.COAL_ORE)).save(pRecipeOutput);
+
+        oreSmelting(pRecipeOutput, NEODYMITE_SMELTABLES, RecipeCategory.MISC, ModItems.NEODYMITE_INGOT.get(), 0.75f, 200, "neodymite");
+        oreBlasting(pRecipeOutput, NEODYMITE_SMELTABLES, RecipeCategory.MISC, ModItems.NEODYMITE_INGOT.get(), 0.75f, 100, "neodymite");
+
 
 
     }
